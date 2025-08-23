b678e954184f8ad30a5be29650e82c9bf1fef22a
yamruka <jesuuuspalacios@gmail.com>
2025-08-23 12:11:47 -0600

    general fixes, the window actually closes and the script ends - added image to readme.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # amp3-python
 an mp3 player coded entirely in python.  
+
+![Powered_By_Pygame](img/pygame_powered.png)
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import tkinter as tk
 import pygame
+import sys
 import os
 
 root = tk.Tk()
 root.title('amp3_player')
 root.geometry("500x300")
-
-pygame.mixer.init()
-
 songlist = tk.Listbox(root, bg="black", fg="white", width=100, height=15)
 songlist.pack()
 
+def on_close():
+    pygame.mixer.quit()
+    root.destroy() 
+    root.destroy()
+    sys.exit()
+    os._exit()
+
+
+root.protocol("WM_DELETE_WINDOW", on_close)
 root.mainloop()
+
+pygame.mixer.init()
+
